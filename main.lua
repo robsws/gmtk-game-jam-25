47375a5e9fad7ef81de4e29921413b0ed296f5b9
Rob Streeting <rostre@Robs-MBP.communityfibre.co.uk>
2025-07-31 15:18:39 +0100

    feat: moving time bar for the drum machine

--- main.lua
+++ main.lua
@@ -4,10 +4,12 @@ NumBeats = 16
 NumInstruments = 7
 NumCellsX = NumBeats
 NumCellsY = NumInstruments
+TempoBps = 2
 
 -- GLOBALS
 
 BeatGrid = {}
+Time = 0
 
 function ResetWindowGlobals()
    WinWidth = love.graphics.getWidth()
@@ -74,41 +76,53 @@ function HandleBeatGridMouseClick(mouse_x, mouse_y)
 end
 
 function DrawGrid()
-   for cell_y = 0, NumCellsY do
-	  for cell_x = 0, NumCellsX do
-		 local top_left = {
-			x = cell_x * CellWidth,
-			y = cell_y * CellHeight + GridTopLeftY
-		 }
-		 local bg_colour = {red = 0, green = 0, blue = 0}
-		 local fg_colour = {red = 1, green = 1, blue = 1}
-		 local cell = BeatGrid[cell_y][cell_x]
-		 if cell.hover then
-			bg_colour = {red = 0.3, green = 0.3, blue = 0.3}
-		 elseif cell.on
-		 then
-			bg_colour = {red = 0, green = 0.8, blue = 0.7}
-		 end
-		 -- background for the cell
-	     love.graphics.setColor(bg_colour.red, bg_colour.green, bg_colour.blue, 1)
-		 love.graphics.rectangle(
-			"fill",
-			top_left.x,
-			top_left.y,
-			CellWidth,
-			CellHeight
-		 )
-		 -- outline for the cell
-		 love.graphics.setColor(fg_colour.red, fg_colour.green, fg_colour.blue, 1)
-		 love.graphics.rectangle(
-			"line",
-			top_left.x,
-			top_left.y,
-			CellWidth,
-			CellHeight
-		 )
-	  end
-   end
+    for cell_y = 0, NumCellsY do
+        for cell_x = 0, NumCellsX do
+            local top_left = {
+                x = cell_x * CellWidth,
+                y = cell_y * CellHeight + GridTopLeftY
+            }
+            local bg_colour = { red = 0, green = 0, blue = 0 }
+            local fg_colour = { red = 1, green = 1, blue = 1 }
+            local cell = BeatGrid[cell_y][cell_x]
+            if cell.hover then
+                bg_colour = { red = 0.3, green = 0.3, blue = 0.3 }
+            elseif cell.on
+            then
+                bg_colour = { red = 0, green = 0.8, blue = 0.7 }
+            end
+            -- background for the cell
+            love.graphics.setColor(bg_colour.red, bg_colour.green, bg_colour.blue, 1)
+            love.graphics.rectangle(
+                "fill",
+                top_left.x,
+                top_left.y,
+                CellWidth,
+                CellHeight
+            )
+            -- outline for the cell
+            love.graphics.setColor(fg_colour.red, fg_colour.green, fg_colour.blue, 1)
+            love.graphics.rectangle(
+                "line",
+                top_left.x,
+                top_left.y,
+                CellWidth,
+                CellHeight
+            )
+        end
+    end
+end
+
+function DrawTimeBar()
+   local secs_to_cross_screen = NumBeats / TempoBps
+   local time_bar_percent = (Time % secs_to_cross_screen) / secs_to_cross_screen
+   local time_bar_x = time_bar_percent * WinWidth
+   local bar_colour = { red = 0.8, green = 0.0, blue = 0.0 }
+   local default_line_width = love.graphics.getLineWidth()
+   love.graphics.setLineWidth(4)
+   love.graphics.setColor(bar_colour.red, bar_colour.green, bar_colour.blue)
+   love.graphics.line(time_bar_x, GridTopLeftY, time_bar_x, WinHeight)
+   love.graphics.setLineWidth(default_line_width)
 end
 
 -- CALLBACKS
@@ -122,10 +136,12 @@ function love.update(dt)
    ResetWindowGlobals()
    ClearBeatGridHoverState()
    HandleBeatGridMouseHover()
+   Time = Time + dt
 end
 
 function love.draw()
    DrawGrid()
+   DrawTimeBar()
 end
 
 function love.mousereleased(x, y, button, istouch, presses)
